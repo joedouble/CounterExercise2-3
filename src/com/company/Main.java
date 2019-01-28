2c636dd3e9c6ea9e16ff6734cc58e0ed27039161
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-28 13:36:30 -0500

    Committng working code from original homework that meets the requirements.

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Exercise 2.3
+
+        int counter = 0;
+
+        while (counter < 100 ) {
+            if (counter == 37) {
+                break;
+            }
+            System.out.println(counter);
+            counter++;
+        }
+    }
+}
